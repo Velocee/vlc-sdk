b7047b09fcbe3be202e77e154e4e680a1952520f
Eli Babila <elibabila@gmail.com>
2015-05-26 12:51:37 +0300

    2.0.77

---
+++ cpres.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# h file
+echo "Searching .h file..."
+hfile=$(find ../velocee-dev-ios/iPhoneSDK -name "VlcSdk.h" | xargs ls -l)
+echo $hfile
+read -p "$(tput setaf 3)Enter to continue & copy, Ctrl-C quits.$(tput sgr0)" ui
+find ../velocee-dev-ios/iPhoneSDK -name "VlcSdk.h" | xargs ls | xargs -I{} cp {} .
+if [[ $? -ne 0 ]]
+then
+    echo "$(tput setaf 1)Error copying h file$(tput sgr0)"
+	exit $?
+fi
+echo "$(tput setaf 2)Ok$(tput sgr0)"
+# bundle
+echo "Searching for bundle..."
+echo $(find /Users/elibabila/Library/Developer/Xcode/DerivedData -type d -name "VlcSdkRes.bundle" | xargs ls -ltrd | tail -1)
+read -p "$(tput setaf 3)Enter to continue & copy (will remove local first), Ctrl-C quits.$(tput sgr0)" ui
+rm -Rf ./VlcSdkRes.bundle
+find /Users/elibabila/Library/Developer/Xcode/DerivedData -type d -name "VlcSdkRes.bundle" | xargs ls -ltrd | tail -1 | grep -o "/Users/.*" | xargs -I{} cp -R {} .
+if [[ $? -ne 0 ]]
+then
+    echo "$(tput setaf 1)Error copying bundle $(tput sgr0)"
+	exit $?
+fi
+echo "$(tput setaf 2)Ok$(tput sgr0)"
+# libvlcsdk
+echo "Searching for sdk file..."
+echo $(find /Users/elibabila/Library/Developer/Xcode/DerivedData -name "libVlcSdk.a" | xargs ls -ltr | grep universal | tail -1)
+read -p "$(tput setaf 3)Enter to continue & copy, Ctrl-C quits.$(tput sgr0)" ui
+find /Users/elibabila/Library/Developer/Xcode/DerivedData -name "libVlcSdk.a" | xargs ls -ltr | grep universal | tail -1 | grep -o "/Users/.*" | xargs -I{} cp {} .
+if [[ $? -ne 0 ]]
+then
+    echo "$(tput setaf 1)Error copying bundle $(tput sgr0)"
+	exit $?
+fi
+echo "$(tput setaf 2)Ok$(tput sgr0)"
+echo "$(tput setaf 2)Done.$(tput sgr0)"
